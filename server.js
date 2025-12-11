e73d360f046e11944732f88606252f22249b1268
AetherLynch <royality73@gmail.com>
2025-12-11 14:11:45 -0700

    Modificacion para que no existan duplicados

--- server.js
+++ server.js
@@ -19,8 +19,13 @@ app.get("/", (req, res) => {
   y el frontend pueda intentar consultarla.
 */
 app.get("/api/characters", (req, res) => {
-  // No devolvemos error, solo indicamos que no hay BD
-  res.json([]);
+  res.json([
+    { name: "El Caballero", role: "Protagonista", description: "Un viajero silencioso que explora Hallownest.", image: "img/vessel.webp" },
+    { name: "Hornet", role: "Guardián", description: "Ágil y mortal, ligada al destino del reino.", image: "img/Hornet_Idle.webp" },
+    { name: "Hollow Knight", role: "Vasija", description: "Creado para contener la infección.", image: "img/hollow.webp" },
+    { name: "Grimm", role: "Jefe", description: "Líder del Troupe Grimm, elegante y mortal.", image: "img/Grimm_Idle.webp" },
+    { name: "Zote", role: "Aventurero", description: "Autoproclamado héroe... con mucha suerte.", image: "img/zote.jpg" }
+  ]);
 });
 
 // Arranque del servidor
